27022265cfa4345af15a5f50def5d96f2de9d585
YU-FENG <simple83815@hotmail.com>
2017-11-13 16:58:03 +0800

    Update example_read_excel.py

--- example_read_excel.py
+++ example_read_excel.py
@@ -9,10 +9,10 @@ def read(excelPath, basePath):
 
     return df_data["path"]
 
-def write(excelPath, basePath, answerList):
+def write(excelPath, basePath, adingList):
 
     # list -> pandas
-    df_answer = pd.DataFrame(answerList, columns=['answer'])
+    df_adingList = pd.DataFrame(adingList, columns=['answer'])
 
     # write
     overwritePath = basePath + excelPath + '.xlsx'
@@ -20,7 +20,7 @@ def write(excelPath, basePath, answerList):
     writer = pd.ExcelWriter(overwritePath, engine='openpyxl')
     writer.book = book
     writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-    df_answer.to_excel(writer, startcol=3, index=False)
+    df_adingList.to_excel(writer, startcol=3, index=False)
     writer.save()
     return 0
 
@@ -36,10 +36,10 @@ data_lists = ['capsaicin_2016_Female_2-1',
 for i_data in data_lists:
     imgList = read(i_data, basePath=basePath)
 
-    # answer
-    answerList = np.zeros(imgList.shape, dtype=np.int32)
+    # adding data
+    adingList = np.zeros(imgList.shape, dtype=np.int32)
 
-    write(i_data, basePath=basePath, answerList=answerList)
+    write(i_data, basePath=basePath, adingList=adingList)
 
 
 
